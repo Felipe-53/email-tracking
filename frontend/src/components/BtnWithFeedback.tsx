f286caab9909d55e5346e67a00c4c64a07c64d5a
Felipe-53 <felipeandresb97@gmail.com>
2023-05-19 23:42:27 -0300

    btn w/ feedback component

---
+++ frontend/src/components/BtnWithFeedback.tsx
@@ -0,0 +1,50 @@
+import React, { PropsWithChildren, useEffect, useRef, useState } from "react";
+
+interface Props {
+  feedbackProps: FeedbackProps;
+  onClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+}
+
+interface FeedbackProps {
+  message: string;
+  timeout: number;
+}
+
+const BtnWithFeedback = ({
+  onClick,
+  feedbackProps,
+  children,
+}: PropsWithChildren<Props>) => {
+  const [copied, setCopied] = useState(false);
+
+  const timerRef = useRef<number | null>(null);
+
+  useEffect(() => {
+    if (copied) {
+      typeof timerRef == "number" && clearTimeout(timerRef);
+      timerRef.current = setTimeout(() => {
+        setCopied(false);
+      }, feedbackProps.timeout);
+    }
+  }, [copied]);
+
+  return (
+    <button
+      className="relative px-2 font-bold btn btn-sm btn-primary my-4"
+      onClick={(e) => {
+        onClick(e);
+        setCopied(true);
+      }}
+    >
+      {children}
+
+      {copied && (
+        <span className="absolute -bottom-6 lowercase text-green-600">
+          {feedbackProps.message}
+        </span>
+      )}
+    </button>
+  );
+};
+
+export { BtnWithFeedback };
